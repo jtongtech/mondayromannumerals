ff992fb8f1bf9d799024aac3bc186add68ff4143
JTong <eldertong@gmail.com>
2017-10-02 13:40:17 -0400

    remove readme and define first test

--- test.rb
+++ test.rb
@@ -3,8 +3,8 @@ require_relative "roman_numerals.rb"
 
 class TestRomanNumerals <Minitest::Test
 	
-	def test_1_equals_1
-		assert_equal(1, 1)
+	def test_1_equals_I
+		assert_equal('I', convert_numbers(1))
     end
     
 end
